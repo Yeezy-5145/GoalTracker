815c26c19a5e864d9a232df6ad06e71cddbb07a4
wasdqwerfzxcv <72960153+wasdqwerfzxcv@users.noreply.github.com>
2022-05-23 15:23:34 +0800

    Delete GoalMou directory

--- GoalMou/javascript/goalList.js
+++
@@ -1,7 +0,0 @@
-function toggleBg() {
-  var body = document.querySelector(".con");
-  body.classList.toggle("active");
-
-  var manageGoal = document.querySelector(".manageGoal");
-  manageGoal.classList.toggle("active");
-}
