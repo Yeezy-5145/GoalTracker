76fa1ec0b3bc93b712addf80079eb88c85d75c37
wasdqwerfzxcv <72960153+wasdqwerfzxcv@users.noreply.github.com>
2022-06-22 11:19:53 +0800

    delete account

--- GoalMou/userProfile.php
+++ GoalMou/userProfile.php
@@ -140,7 +140,6 @@ if(isset($_POST['deleteAccount'])) {
 	$sql2 = "DELETE FROM user WHERE user_id=$id ";
 	mysqli_query($link, $sql2);
 	echo "<script>alert('Account deleted successfully.');</script>";
-	$success = False;
 	header("Refresh:0 url=goal.php");
 }
 
